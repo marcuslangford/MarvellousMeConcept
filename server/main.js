f7ba1b583b6c5aab65294ccc13b493e7e4342bdb
marcuslangford <up777600@myport.ac.uk>
2019-02-09 10:45:07 +0000

    Update main.js

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ Meteor.startup(() => {
 
 export const Notes = new Mongo.Collection('notes');
 export const Notifications = new Mongo.Collection('notifications');
-export const Activites = new Mongo.Collection('activites');
+export const Activities = new Mongo.Collection('activities');
 export const Saved = new Mongo.Collection('saved');
 export const Messages = new Mongo.Collection('messages');
 export const Badges = new Mongo.Collection('badges');
