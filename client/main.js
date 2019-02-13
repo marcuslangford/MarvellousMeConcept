c5795b38179315ccf24e06ea484c2969e73f3034
marcuslangford <up777600@myport.ac.uk>
2019-02-13 18:44:56 +0000

    Added multiple students

--- client/main.js
+++ client/main.js
@@ -26,16 +26,16 @@ Template.body.helpers({
 
     //Update items so they are no longer new and add an item to the notifications collection
     newActivities.forEach(function(item){
+      console.log(item.student)
       Activities.update(
         {_id: item._id},
         {
           $set: {"new": false}
         }
       )
-      Notifications.insert({"type" : "activity","text" : "Marcus Learned an Activity", "obID" : item._id})
+      Notifications.insert({"type" : "activity","text" : item.student +  " Learned an Activity", "obID" : item._id})
     })
     newMessages.forEach(function(message){
-      console.log(message._id._str)
       Messages.update(
         {_id: message._id},
         {
@@ -72,36 +72,47 @@ Template.body.helpers({
 
   activities(){
 
-
     var start =  new Date();
     start.setHours(0,0,0,0);
     var end = new Date();
     end.setHours(23,59,59,999);
+    currentStudent = Session.get("student");
+
     if(Activities.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }}).count()!= 0){
-      Session.set("atotal", Activities.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }}).count());
+      Session.set("atotal", Activities.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }, student : currentStudent}).count());
       console.log(Session.get("atotal"))
       getTotal();
     }
-
+    y = document.getElementById('aempty');
     switch(Session.get("anewTime")){
 
       case '1':
+        if(Activities.find({createdAt: {$gte: start, $lt: end}}).count() == 0){
+          y.style.display = "block";
+          y.innerHTML = "No new Activities today! "
+        }
+        else{y.style.display = "none"}
 
         if(Session.get("subjectFilter") != null){
           return Activities.find (
             {
               createdAt: {$gte: start, $lt: end},
-              subject  : Session.get("subjectFilter")
-            }
-          );
+              subject  : Session.get("subjectFilter"),
+              student : currentStudent
+            });
 
         } else{
 
-          return Activities.find({createdAt: {$gte: start, $lt: end}});
+          return Activities.find (
+            {
+              createdAt: {$gte: start, $lt: end},
+              student  : currentStudent
+            });
         }
 
 
       case '2':
+        y.style.display = "none";
         Session.set("atotal", Activities.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }}).count());
         getTotal();
         if(Session.get("subjectFilter") != null){
@@ -109,37 +120,42 @@ Template.body.helpers({
           return Activities.find (
               {
                 createdAt: { $gt: new Date(start - (7*24*60*60*1000)) },
-                subject  : Session.get("subjectFilter")
+                subject  : Session.get("subjectFilter"),
+                student : currentStudent
               }
           );
-        }else{return Activities.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }})}
+        }else{return Activities.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }, student : currentStudent}, {sort : {createdAt: -1}})}
 
       case '3':
+        y.style.display = "none";
         if(Session.get("subjectFilter") != null){
-          return Activities.find ({subject  : Session.get("subjectFilter")});
+          return Activities.find ({subject  : Session.get("subjectFilter"), student : currentStudent});
           break;
-        }else{return Activities.find({})};
+        }else{return Activities.find({student : currentStudent}, {sort : {createdAt: -1}})};
 
       case '4':
+        y.style.display = "none";
         return Activities.find ({ saved: true});
 
       case '5':
-      dateSelected = document.getElementById('timePicker').value;
+        y.style.display = "none";
+        dateSelected = document.getElementById('timePicker').value;
 
-      document.getElementById('timePicker').value = "";
-      if(Session.get("subjectFilter") != null){
+        document.getElementById('timePicker').value = "";
+        if(Session.get("subjectFilter") != null){
 
-        return Activities.find (
-            {
-              createdAt: { $eq: new Date(dateSelected) },
-              subject  : Session.get("subjectFilter")
-            }
-        );
+          return Activities.find (
+              {
+                createdAt: { $eq: new Date(dateSelected) },
+                subject  : Session.get("subjectFilter"),
+                student : currentStudent
+              }
+          );
 
-      }else{
-        return Activities.find({createdAt: { $eq: new Date(dateSelected) }})
+        }else{
+          return Activities.find({createdAt: { $eq: new Date(dateSelected) }, student : currentStudent})
 
-      }
+        }
 
     }
 
@@ -157,24 +173,32 @@ Template.body.helpers({
     start.setHours(0,0,0,0);
     var end = new Date();
     end.setHours(23,59,59,999);
-    Session.set("btotal", Badges.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }}).count())
+    Session.set("btotal", Badges.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }, student : currentStudent}).count())
     getTotal();
-
+    x = document.getElementById('bempty');
     switch(Session.get("bnewTime")){
 
       case '1':
 
+        if(Badges.find({createdAt: {$gte: start, $lt: end}}).count() == 0){
+          x.style.display = "block";
+          x.innerHTML = "No new Badges today!"
+        }
+
         return Badges.find({createdAt: {$gte: start, $lt: end}});
 
       case '2':
+        x.style.display = "none";
 
         return Badges.find({createdAt: { $gt: new Date(start - (7*24*60*60*1000)) }});
 
       case '3':
+        x.style.display = "none";
 
         return Badges.find({});
 
       case '4':
+        x.style.display = "none";
 
         return Badges.find({"saved" : true});
 
@@ -189,7 +213,9 @@ Template.notification.events({
     Notifications.remove(this._id);
     return false;
   }
-})
+});
+
+
 
 Template.message.events({
   'click .delete-message': function(){
@@ -199,11 +225,14 @@ Template.message.events({
 })
 
 
-
-Template.ainputForm.onCreated(function(){
+Template.body.onCreated(function(){
   Session.set("anewTime", "1" );
   Session.set("bnewTime", "1" );
   Session.set("subjectFilter", null);
+  Session.set("student", "James");
+
+
+
 });
 
 Template.ainputForm.events({
@@ -276,9 +305,9 @@ Template.registerHelper('formatDate', function(date) {
 });
 
 
+
 Template.activity.events({
   'click .save-activity': function(){
-    event.target.className = "save-activity material-icons right saved";
 
     Activities.update(
       {_id: this._id},
