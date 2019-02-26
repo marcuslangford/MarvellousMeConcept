f88a9880b3a4beb66edf4d2f5ac2f29c865acbba
marcuslangford <up777600@myport.ac.uk>
2019-02-26 19:42:10 +0000

    Update main.js

--- server/main.js
+++ server/main.js
@@ -10,3 +10,6 @@ export const Activities = new Mongo.Collection('activities');
 export const Saved = new Mongo.Collection('saved');
 export const Messages = new Mongo.Collection('messages');
 export const Badges = new Mongo.Collection('badges');
+export const HomeTasks = new Mongo.Collection('hometasks');
+export const Events = new Mongo.Collection('events');
+export const Students = new Mongo.Collection('students');
